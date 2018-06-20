e66fa546c6a96e009a25b0e801c0ff581245d008
jaivonmassena <jaivonmassena@gmail.com>
2018-06-20 19:10:24 +0200

    sprint completed

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -1,14 +1,27 @@
-/**
- * LS-8 v2.0 emulator skeleton code
- */
-const LDI = 0b10011001
-const PRN = 0b01000011
-const HLT = 0b00000001
-const MUL = 0b10101010
-const DIV = 0b10101011;
+
+const LDI = 0b10011001;
+const PRN = 0b01000011;
+const HLT = 0b00000001;
+const MUL = 0b10101010;
+const ADD = 0b10101000;
+const POP = 0b01001100;
+const PUSH = 0b01001101;
+const CALL = 0b01001000;
+const JMP = 0b01010000;
+const DEC = 0b01111001;
+const RET = 0b00001001;
+const CMP = 0b10100000;
+const JEQ = 0b01010001;
+const JNE = 0b01010010;
+const SP = 7;
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
+
+const FL_L = 0x1 << 2;
+const FL_G = 0x1 << 1;
+const FL_E = 0x1 << 0;
+
 class CPU {
 
     /**
@@ -18,9 +31,11 @@ class CPU {
         this.ram = ram;
 
         this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
-
+        this.reg[SP] = 0xF4
         // Special-purpose registers
         this.PC = 0; // Program Counter
+        this.reg.FL = 0b00000000;
+
     }
 
     /**
@@ -60,8 +75,13 @@ class CPU {
         switch (op) {
             case 'MUL':
                 // !!! IMPLEMENT ME
-                this.reg[regA] = regA * regB;
-                break;
+                this.reg[regA] = this.reg[regA] * this.reg[regB];
+            break;
+            case 'ADD':
+                this.reg[regA] += this.reg[regB];
+            break;
+
+
         }
     }
 
@@ -73,50 +93,110 @@ class CPU {
         // from the memory address pointed to by the PC. (I.e. the PC holds the
         // index into memory of the instruction that's about to be executed
         // right now.)
+        const IR = this.ram.read(this.PC);
 
-        // !!! IMPLEMENT ME
-        const IR = this.ram.read(this.reg.PC)
         // Debugging output
         //console.log(`${this.PC}: ${IR.toString(2)}`);
 
         // Get the two bytes in memory _after_ the PC in case the instruction
         // needs them.
+        const pushV= (v) =>{
+          this.reg[SP]--;
+          this.ram.write(this.reg[SP], v);
+        }
+        const pop = () => {
 
-        // !!! IMPLEMENT ME
-        const operandA = this.ram.read(this.reg.PC + 1);
-        const operandB = this.ram.read(this.reg.PC + 2);
+          this.reg[SP]++;
+          return this.ram.read(this.reg[SP]);;
+        };
+        const operandA = this.ram.read(this.PC + 1);
+        const operandB = this.ram.read(this.PC + 2);
+
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
+        let pcAdvance = true;
+        switch(IR) {
 
-        // !!! IMPLEMENT ME
-        switch(IR){
           case LDI:
-            this.stopClock();
+            this.reg[operandA] = operandB;
+            break;
+          case MUL:
+            this.alu('MUL', operandA, operandB);
+            break;
+          case ADD:
+            this.alu('ADD', operandA, operandB);
+            break;
+          case PUSH:
+            pushV(this.reg[operandA]);
+            break;
+          case POP:
+            this.reg[operandA] = pop();
+            break;
+          case CALL:
+            pcAdvance = false;
+            pushV(this.reg.PC + 2);
+            this.reg.PC = this.reg[operandA];
+            break;
+          case JMP:
+            pcAdvance = false;
+            this.reg.PC = this.reg[operandA];
+            break;
+          case RET:
+            pcAdvance = false;
+            this.reg.PC = this.ram.read(this.reg[SP]);
+            this.reg[SP]++;
+            break;
+
+            this.reg.FL = pop();
+            this.reg.PC = pop();
             break;
           case PRN:
-            console.log(this.reg[operandA])
+            console.log(this.reg[operandA]);
             break;
-          case 'MUL':
-            this.reg[regA] = operandA * operandB;
+
+          case CMP:
+            if (this.reg[operandA] === this.reg[operandB]) this.reg.FL |= FL_E;
+            else this.reg.FL &= ~FL_E;
+            if (this.reg[operandA] < this.reg[operandB]) this.reg.FL |= FL_L;
+            else this.reg.FL &= ~FL_L;
+            if (this.reg[operandA] > this.reg[operandB]) this.reg.FL |= FL_G;
+            else this.reg.FL &= ~FL_G;
             break;
+          case JEQ:
+            if ((this.reg.FL &= 0b00000001) === 0b1) {
+              pcAdvance = false;
+              this.reg.PC = this.reg[operandA];
+            }
+            break;
+          case JNE:
+            if ((this.reg.FL &= 0b00000001) === 0b0) {
+              pcAdvance = false;
+              this.reg.PC = this.reg[operandA];
+            }
+            break;
+          // case ST:
+          //   this.ram.write(this.reg[operandA], this.reg[operandB]);
+          //   break;
           case HLT:
             this.stopClock();
             break;
+           default:
+              console.log("Unknown instruction: " + IR.toString(2));
+              this.stopClock();
+              return;
 
-          default:
-            console.error(`Error`)
-            this.stopClock();
-            return;
         }
+
         // Increment the PC register to go to the next instruction. Instructions
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
         // instruction byte tells you how many bytes follow the instruction byte
         // for any particular instruction.
-
-        // !!! IMPLEMENT ME
-        const instLen = (IR >> 6) + 1;
-        this.PC += instLen;
+        if(pcAdvance){
+          const instLen = (IR >> 6) + 1;
+          this.PC += instLen;
+      }
     }
+
 }
 
 module.exports = CPU;
--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,39 +1,47 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
+const fs = require('fs');
 
 /**
  * Load an LS8 program into memory
  *
  * TODO: load this from a file on disk instead of having it hardcoded
  */
-function loadMemory() {
+function loadMemory(cpu, filename) {
 
+    const content = fs.readFileSync(filename, 'utf-8')
     // Hardcoded program to print the number 8 on the console
+    const lines = content.trim().split(/[\r\n]+/g);
+    const program = [];
 
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ];
+    for (let line of lines) {
+      const val = parseInt(line, 2);
+
+      if (isNaN(val)){
+        continue;
+      }
+      program.push(val);
+    }
+
 
     // Load the program into the CPU's memory a byte at a time
     for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
+        cpu.poke(i, program[i]);
     }
 }
 
 /**
  * Main
  */
-
+if(process.argv.length != 3){
+  console.log("usage: ls8 filename");
+  process.exit[1];
+}
 let ram = new RAM(256);
 let cpu = new CPU(ram);
 
-// TODO: get name of ls8 file to load from command line
 
-loadMemory(cpu);
+
+loadMemory(cpu, process.argv[2]);
 
 cpu.startClock();
